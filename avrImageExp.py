6bcbd07c7bee121443d5c769f9bcc1b705db2703
Barsleebi aka shlomojohn <90161415+barsleebi@users.noreply.github.com>
2023-01-14 19:46:54 +0000

    Update avrImageExp.py

--- avrImageExp.py
+++ avrImageExp.py
@@ -2,7 +2,7 @@
 Author : Barsleebi Thomas John
 Date : 09-08-2021
 Function : This will use avamar in-build modify snapups module to generate forced backup expiration scripts. 
-These scripts will be then copied /tmp on the avamar server and executed. 
+These scripts will be then copied to /tmp on the avamar server and executed. 
 Do note this will just change the expiration date of all backup images actual deletion will start once the garbage collection is started.
 """
 import os, sys, re, string, time, paramiko
